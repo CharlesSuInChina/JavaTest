2288a37c513651df7e2c2b9c7b4edf87096f013b
xjzhang0504 <1072540920@qq.com>
2017-12-22 10:09:44 +0800

    add source

--- src/LocationHandler.java
+++ src/LocationHandler.java
@@ -13,20 +13,26 @@ import java.util.Map;
 
 public class LocationHandler {
     static Map<String,Integer> addressMap = new HashMap<String,Integer>();
-    static String UIRTByBlockCountFilePath = "result/" + new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()) + "uirtByBlockCount.txt";
-    private static double minLng = 112.0;
-    private static double maxLng = 120.0;
-    private static double minLat = 35.0;
-    private static double maxLat = 43.0;
-    private static double stepSize = 50.0; // km
 
+//    private static double minLng = 112.0;
+//    private static double maxLng = 120.0;
+//    private static double minLat = 35.0;
+//    private static double maxLat = 43.0;
+
+    private static double minLng = 110.0;
+    private static double maxLng = 123.0;
+    private static double minLat = 31.0;
+    private static double maxLat = 43.0;
+    private static double stepSize = 200.0; // km
+    static String UIRTByBlockCountFilePath = "result/" + new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()) + stepSize + "uirtBlock.txt";
 
     public static void main(String[] args){
         //calculate block
         Map<String, Double> modeMap = new LocationHandler().getBlockMode(minLng, minLat, maxLng, maxLat);
         System.out.println(modeMap.toString());
         // first step read uirt.txt
-        new LocationHandler().readFileByLines("source/uirt(3).txt", modeMap);
+       // new LocationHandler().readFileByLines("source/uirt(3).txt", modeMap);
+        new LocationHandler().readFileByLines("source/uirtAllLo.txt", modeMap);
         System.out.println("the request count is : " + HttpClientExample.requestCount);
     }
 
@@ -132,28 +138,31 @@ public class LocationHandler {
         //calculate lng step size(degree)
         Double radians = Math.toRadians(lat);//转成弧度
         Double cosValue = Math.cos(radians);
-        Double lngKmPerDu = 111.314 * cosValue;
-        Double stepSizeLng = stepSize / lngKmPerDu;
+        Double lngKmPerDu = 111.314 * cosValue;//经度   一度所对应的长度（单位：km）
+        Double stepSizeLng = stepSize / lngKmPerDu;//经度上步长（单位：度）
         return  stepSizeLng;
     }
 
     private Map<String, Double> getBlockMode(Double minLng, Double minLat, Double maxLng, Double maxLat){
-        Double stepSizeLng = calculateStepSizeLng(minLat);
+        Double stepSizeLng = calculateStepSizeLng(minLat);//最小纬度下的经度步长
 
         //calculate lat step size(degree)
-        Double stepSizeLat = stepSize / 110.95;
+        Double stepSizeLat = stepSize / 110.95;//纬度步长，定值
 
+        double latCount = Math.ceil((maxLat-minLat)/stepSizeLat);
+        System.out.println("纵向分割个数："+latCount);
         //calculate rank count
         Double lngDiff = maxLng - minLng;
-        double lngDiffCount = Math.ceil(lngDiff / stepSizeLng);
+        double lngDiffCount = Math.ceil(lngDiff / stepSizeLng);//向上取整，横向块数
+        System.out.println("横向分割个数："+lngDiffCount);
 
         Map<String, Double> resultMap = new HashMap<String, Double>();
         resultMap.put("minLng", minLng);
         resultMap.put("minLat", minLat);
         resultMap.put("maxLng", maxLng);
         resultMap.put("maxLat", maxLat);
-        resultMap.put("stepSizeLat", stepSizeLat);
-        resultMap.put("lngDiffCount", lngDiffCount);
+        resultMap.put("stepSizeLat", stepSizeLat);//纬度步长
+        resultMap.put("lngDiffCount", lngDiffCount);//横向块数
         return resultMap;
     }
 
@@ -161,13 +170,13 @@ public class LocationHandler {
         Double minLng = map.get("minLng");
         Double minLat = map.get("minLat");
         Double stepSizeLng = calculateStepSizeLng(lat);//计算经度的步长
-        Double stepSizeLat = map.get("stepSizeLat");
+        Double stepSizeLat = map.get("stepSizeLat");//纬度步长，定值
         Double lngDiffCount = map.get("lngDiffCount");
 
-        double xCoordinate = Math.floor((lng - minLng) / stepSizeLng);
-        double yCoordinate = Math.floor((lat - minLat) / stepSizeLat);
+        double xCoordinate = Math.floor((lng - minLng) / stepSizeLng);//经度坐标
+        double yCoordinate = Math.floor((lat - minLat) / stepSizeLat);//纬度坐标
 
-        double blockCount = yCoordinate * lngDiffCount + xCoordinate;
+        double blockCount = yCoordinate * lngDiffCount + xCoordinate;//块编码
         return blockCount;
     }
 
@@ -197,7 +206,7 @@ public class LocationHandler {
         String countyName = "";
         if (detailLocation.contains("涞水")){
             countyName = "涞水县，";
-        }else if(detailLocation.contains("涞水")){
+        }else if(detailLocation.contains("怀来")){
             countyName = "怀来县，";
         }else if(detailLocation.contains("张北")){
             countyName = "张北县，";
@@ -215,7 +224,7 @@ public class LocationHandler {
 
     private String checkCityName(String detailLocation){
         String cityName = "";
-        if(detailLocation.contains("张家口")){
+        if(detailLocation.contains("张家口")){//河北
             cityName = "张家口市，";
         }else if(detailLocation.contains("石家庄")){
             cityName = "石家庄市，";
@@ -241,6 +250,96 @@ public class LocationHandler {
             cityName = "涿州市，";
         }else if(detailLocation.contains("霸州")){
             cityName = "霸州市，";
+        }else if(detailLocation.contains("郑州")){//河南
+            cityName = "郑州市，";
+        }else if(detailLocation.contains("安阳")){
+            cityName = "安阳市，";
+        }else if(detailLocation.contains("鹤壁")){
+            cityName = "鹤壁市，";
+        }else if(detailLocation.contains("焦作")){
+            cityName = "焦作市，";
+        }else if(detailLocation.contains("开封")){
+            cityName = "开封市，";
+        }else if(detailLocation.contains("漯河")){
+            cityName = "漯河市，";
+        }else if(detailLocation.contains("洛阳")){
+            cityName = "洛阳市，";
+        }else if(detailLocation.contains("南阳")){
+            cityName = "南阳市，";
+        }else if(detailLocation.contains("平顶山")){
+            cityName = "平顶山市，";
+        }else if(detailLocation.contains("濮阳")){
+            cityName = "濮阳市，";
+        }else if(detailLocation.contains("三门峡")){
+            cityName = "三门峡市，";
+        }else if(detailLocation.contains("商丘")){
+            cityName = "商丘市，";
+        }else if(detailLocation.contains("新乡")){
+            cityName = "新乡市，";
+        }else if(detailLocation.contains("信阳")){
+            cityName = "信阳市，";
+        }else if(detailLocation.contains("许昌")){
+            cityName = "许昌市，";
+        }else if(detailLocation.contains("周口")){
+            cityName = "周口市，";
+        }else if(detailLocation.contains("驻马店")){
+            cityName = "驻马店市，";
+        }else if(detailLocation.contains("济南")){//山东
+            cityName = "济南市，";
+        }else if(detailLocation.contains("青岛")){
+            cityName = "青岛市，";
+        }else if(detailLocation.contains("滨州")){
+            cityName = "滨州市，";
+        }else if(detailLocation.contains("德州")){
+            cityName = "德州市，";
+        }else if(detailLocation.contains("东营")){
+            cityName = "东营市，";
+        }else if(detailLocation.contains("菏泽")){
+            cityName = "菏泽市，";
+        }else if(detailLocation.contains("济宁")){
+            cityName = "济宁市，";
+        }else if(detailLocation.contains("莱芜")){
+            cityName = "莱芜市，";
+        }else if(detailLocation.contains("聊城")){
+            cityName = "聊城市，";
+        }else if(detailLocation.contains("临沂")){
+            cityName = "临沂市，";
+        }else if(detailLocation.contains("日照")){
+            cityName = "日照市，";
+        }else if(detailLocation.contains("泰安")){
+            cityName = "泰安市，";
+        }else if(detailLocation.contains("潍坊")){
+            cityName = "潍坊市，";
+        }else if(detailLocation.contains("威海")){
+            cityName = "威海市，";
+        }else if(detailLocation.contains("烟台")){
+            cityName = "烟台市，";
+        }else if(detailLocation.contains("枣庄")){
+            cityName = "枣庄市，";
+        }else if(detailLocation.contains("淄博")){
+            cityName = "淄博市，";
+        }else if(detailLocation.contains("太原")){//山西
+            cityName = "太原市，";
+        }else if(detailLocation.contains("长治")){
+            cityName = "长治市，";
+        }else if(detailLocation.contains("大同")){
+            cityName = "大同市，";
+        }else if(detailLocation.contains("晋城")){
+            cityName = "晋城市，";
+        }else if(detailLocation.contains("晋中")){
+            cityName = "晋中市，";
+        }else if(detailLocation.contains("临汾")){
+            cityName = "临汾市，";
+        }else if(detailLocation.contains("吕梁")){
+            cityName = "吕梁市，";
+        }else if(detailLocation.contains("朔州")){
+            cityName = "朔州市，";
+        }else if(detailLocation.contains("忻州")){
+            cityName = "忻州市，";
+        }else if(detailLocation.contains("阳泉")){
+            cityName = "阳泉市，";
+        }else if(detailLocation.contains("运城")){
+            cityName = "运城市，";
         }
         return  cityName;
     }
